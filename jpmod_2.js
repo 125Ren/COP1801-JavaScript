657ec572c51106cfa9d03973ac6e5ac98b37c8f2
NovGrad24 <150361480+125Ren@users.noreply.github.com>
2024-02-17 15:08:10 -0500

    Add files via upload
    README.md

---
+++ jpmod_2.js
@@ -0,0 +1,21 @@
+var myheading = "<h1>This is My Program Webpage!</h1>";
+const myPI = 3.1415926;
+let person = "Sam";
+var myFavNum= 33;
+var circleRadius= Math.sqrt(3421.194341 / 3.1415926);
+let myArea = 3.1415926 * 33 ** 2;
+linktag = "<a href=\"http://sotd.us/qrentellaadams1/indexone.html\"> Click Here</a>",
+sometext = "This text can be affected by other statements.",
+begineffect = "<em>",
+endeffect = "</em>",
+beginpara = "<p>",
+endpara = "</p>";
+
+//
+
+document.write(myheading)
+document.write(begineffect + sometext + endeffect)
+document.write(beginpara + linktag + endpara)
+document.write(beginpara + sometext + endpara)
+
+//
