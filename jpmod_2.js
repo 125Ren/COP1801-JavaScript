2814812f1f2e8ac574d3d5f1a3e5c70ad99e11e4
NovGrad24 <150361480+125Ren@users.noreply.github.com>
2024-02-26 13:20:03 -0500

    Add files via upload
    README.md

--- jpmod_2.js
+++ jpmod_2.js
@@ -1,14 +1,5 @@
 var linktag = "<a href=\"http://sotd.us/qrentellaadams1/indexone.html\"> Click Here</a>";
-var myheading = "<h3>JavaScript Program Webpage!</h3>";
-
-let a; // Now a is undefined
-a = 5;    // Now a is a Number
-a = "Hello Sam";    // Now a is a String
-const myPI = 3.1415926;
-var myFavNum = 33;
-let myArea; // Now myArea is undefined
-myArea =  3.1415926 * 33 ** 2; // Now b is a Number
-
+var myheading = "<h3>JavaScript Program Webpage!</h3>",
 sometext = "This text can be affected by other statements.",
 begineffect = "<strong>",
 endeffect = "</strong>",
@@ -18,8 +9,7 @@ endpara = "</p>";
 //
 
 document.write(myheading)
-document.write(begineffect + "Hello Sam, You entered 33 as your favorite number." + endeffect + "<br>")
-document.write(beginpara + "If that was the radius of a circle, 33" + endpara + "<br>")
-document.write("<em>" + "The circle’s area would be 3421.194341"  + "</em>"+ "<br>")
+document.write(begineffect + endeffect)
+document.write(beginpara + endpara)
 
 //
