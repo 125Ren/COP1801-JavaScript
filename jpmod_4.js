d1256e58f18fea98ab826bc419c8a6db4ce1b209
NovGrad24 <150361480+125Ren@users.noreply.github.com>
2024-03-02 21:43:47 -0500

    Add files via upload
    README.md

---
+++ jpmod_4.js
@@ -0,0 +1,17 @@
+var linktag = "<a href=\"http://sotd.us/qrentellaadams1/indexone.html\"> Click Here</a>";
+let myheading = "<h1>Calculating A Tip</h1>"
+let mytext = '<p>Thank you for making WeWannaBe your dining choice.</p>';
+const Poor = 10;
+const Okay = 15;
+const Great = 20;
+function decimal(x, f) {
+  return Number.parseFloat(x).toDecimal(f);
+begineffect = "<strong>",
+endeffect = "</strong>";
+
+//
+
+document.write(myheading)
+document.write(mytext);
+
+//
